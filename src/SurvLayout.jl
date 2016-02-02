de5912e1590a762bd5b3e6f5ace4264da88a3e93
Zhenning Yu <ContaTP@users.noreply.github.com>
2016-02-02 15:10:03 -0500

    Update SurvLayout.jl

--- src/SurvLayout.jl
+++ src/SurvLayout.jl
@@ -1,7 +1,14 @@
 # transform to Dataframe
 function SurvLayout(survobj::KMSurv)
-    Survtable = DataFrame(Time=survobj.t, Total=survobj.n, Censored=survobj.c,
-                          Event=survobj.d, Survival_Prob=survobj.d_n, Survival=survobj.surv_func, Std_Error=survobj.std)
+    if !isnull(survobj.conf_interval)
+        conf_interval = get(survobj.conf_interval)
+        Survtable = DataFrame(Time=survobj.t, Total=survobj.n, Censored=survobj.c,
+                             Event=survobj.d, Survival_Prob=survobj.d_n, Survival=survobj.surv_func, Std_Error=survobj.std,
+                             LowerCI=conf_interval.lower, UpperCI=conf_interval.upper)
+    else
+        Survtable = DataFrame(Time=survobj.t, Total=survobj.n, Censored=survobj.c,
+                             Event=survobj.d, Survival_Prob=survobj.d_n, Survival=survobj.surv_func, Std_Error=survobj.std)
+    end
     return Survtable
 end
 
@@ -15,3 +22,4 @@ function SurvLayout(survobj::MRLSurv)
     Survtable = DataFrame(Time=survobj.t, Mean_Residual_Life=survobj.mean_resid_life)
     return Survtable
 end
+
