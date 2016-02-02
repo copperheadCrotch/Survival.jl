85659287a0f83a97e1df7b34922dd0e3bedbad8b
Zhenning Yu <ContaTP@users.noreply.github.com>
2016-02-02 15:09:48 -0500

    Update NelsonAalen.jl

--- src/NelsonAalen.jl
+++ src/NelsonAalen.jl
@@ -16,10 +16,13 @@ function NAEst(survobj::SurvObject)
         unshift!(t, 0)
         println("Add 0 to time points")
     end
-    c = [count(i->(i==0), event[findin(time, j)]) for j in t]
     n = [count(i->(i>=j),time) for j in t]
+    # c = sort(unique(time[findin(event, 0)]))
+    c = [count(i->(i==0), event[findin(time, j)]) for j in t]
     d = [sum(event[findin(time, i)]) for i in t]
-    d_n = [1-d[i]/n[i] for i = 1:length(n)]
+    # d_n = map(i->1-d[i]/n[i], [i for i = 1:length(n)])
+    d_n = 1 - d./n
     cum_hazard = cumsum(d_n)
     return NASurv(t, n, c, d, d_n, cum_hazard)
 end
+
