8ce45ac19753813b54f68a4d0f51141471a6e166
Zhenning Yu <yuzhenning.bio@gmail.com>
2017-01-19 11:01:44 -0500

    update

--- src/MeanResidLife.jl
+++ src/MeanResidLife.jl
@@ -1,21 +1,14 @@
-# MRLSurv type
-immutable MRLSurv <: NonParaSurv
-    t::Vector{Float64}
-    mean_resid_life::Vector{Float64}
-end
-
-
 # calculate mean residual life for censored data
-function MeanResidLife(survobj::KMSurv)
-    censor = find(survobj.c)
-    time = survobj.t
+function MeanResidLife(survobj::Surv)
+    censor = find(survobj.event)
+    time = survobj.time
     surv_func = survobj.surv_func
 
     if sum(censor) == 0
         error("There is no censored objects in the data!")
     end
 
-    time_interval = diff(survobj.t)
+    time_interval = diff(survobj.time)
     unshift!(time_interval, 0)
     area = time_interval .* surv_func # calculate area under the curve
     area_under_curve = reverse(cumsum(reverse(area)))
