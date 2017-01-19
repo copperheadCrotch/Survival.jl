1be10aa4d9d6fdc694ba81b96c0004fc3e70b529
Zhenning Yu <yuzhenning.bio@gmail.com>
2017-01-19 11:02:49 -0500

    update

--- src/MeanResidLife.jl
+++ src/MeanResidLife.jl
@@ -2,13 +2,13 @@
 function MeanResidLife(survobj::Surv)
     censor = find(survobj.event)
     time = survobj.time
+    time_interval = diff(time)
     surv_func = survobj.surv_func
 
     if sum(censor) == 0
         error("There is no censored objects in the data!")
     end
 
-    time_interval = diff(survobj.time)
     unshift!(time_interval, 0)
     area = time_interval .* surv_func # calculate area under the curve
     area_under_curve = reverse(cumsum(reverse(area)))
